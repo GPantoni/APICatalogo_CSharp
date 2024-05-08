3350201e3bd7ca8e49d03d6caf4c9c248daf33eb
Glauco Pantoni <glaucopantoni@gmail.com>
2024-05-08 10:39:21 -0300

    Aplica exception filter + log de erros + otimizações de código

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,23 +1,33 @@
 using System.Text.Json.Serialization;
 using APICatalogo.Context;
-using APICatalogo.Extensions;
+using APICatalogo.Filters;
+using APICatalogo.Logging;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
+builder.Services.AddControllers(options =>
+        options.Filters.Add(typeof(ApiExceptionFilter)))
     .AddJsonOptions(options =>
         options.JsonSerializerOptions
             .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddScoped<ApiLoggingFilter>();
+builder.Logging.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
+{
+    LogLevel = LogLevel.Information
+}));
 
 var app = builder.Build();
 
@@ -26,7 +36,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.ConfigureExcepetionHandler();
 }
 
 app.UseHttpsRedirection();
