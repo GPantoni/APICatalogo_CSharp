c8a0e4caef43d3b5f9aa72512c76589efaf67257
Glauco Pantoni <glaucopantoni@gmail.com>
2024-05-14 10:42:33 -0300

    feat: cria classe pagedList para fazer a paginação de produtos

--- APICatalogo/Repositories/IProdutoRepository.cs
+++ APICatalogo/Repositories/IProdutoRepository.cs
@@ -5,6 +5,6 @@ namespace APICatalogo.Repositories;
 
 public interface IProdutoRepository : IRepository<Produto>
 {
-    IEnumerable<Produto> GetProdutos(ProdutosParameters produtosParameters);
+    PagedList<Produto> GetProdutos(ProdutosParameters produtosParameters);
     IEnumerable<Produto> GetProdutosPorCategoria(int id);
 }
